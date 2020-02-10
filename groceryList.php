d06eddaffbab871ce219e5e841dfc82e379a4c5a
morerebe <59507960+morerebe@users.noreply.github.com>
2020-02-10 13:09:27 -0500

    Updated goceryList display php

--- groceryList.php
+++ groceryList.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <title>Your Shopping List</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<h1>Grocery Shopping List</h1>
+<h1>Your Grocery Shopping List</h1>
+<p>Have fun shopping!</p>
 <?php
 
 $db = new PDO("mysql:host=172.31.22.43;dbname=Rebecca100157685", "Rebecca100157685","TOqN7o1T_n"); // connect to the DB
@@ -24,7 +26,7 @@ echo '<table class="table table-striped"><thead class="thead-dark"><th>Item</th>
 
 // loop through data and display results
 
-foreach ($musicians as $value) {
+foreach ($groceryList as $value) {
     echo '<tr><td>' . $value['item'] . '</td>
             <td>' . $value['categories'] . '</td>
             <td>' . $value['quantity'] . '</td>
